858162e872c89dc12e403d99fb8ce16b204dc74a
akshataGit <109665990+akshataGit@users.noreply.github.com>
2024-01-25 16:24:52 +0530

    Update Json1.java

--- Json1.java
+++ Json1.java
@@ -26,6 +26,38 @@ public class JsonParsingExample {
 
 
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Main {
+    public static void main(String[] args) {
+        String jsonString = "{\"data\":[{\"MainId\":1111,\"firstName\":\"Sherlock\",\"lastName\":\"Homes\",\"categories\":[{\"CategoryID\":1,\"CategoryName\":\"Example\"}]},{\"MainId\":122,\"firstName\":\"James\",\"lastName\":\"Watson\",\"categories\":[{\"CategoryID\":2,\"CategoryName\":\"Example2\"}]}],\"messages\":[],\"success\":true}";
+
+        JSONObject jsonObject = new JSONObject(jsonString);
+        JSONArray dataArray = jsonObject.getJSONArray("data");
+
+        // Find the object with MainId 122
+        JSONObject targetObject = dataArray
+                .toList()
+                .stream()
+                .filter(obj -> ((JSONObject) obj).getInt("MainId") == 122)
+                .map(JSONObject::new)
+                .findFirst()
+                .orElse(null);
+
+        if (targetObject != null) {
+            JSONArray categoriesArray = targetObject.getJSONArray("categories");
+            JSONObject categoryObject = categoriesArray.getJSONObject(0);
+            String categoryName = categoryObject.getString("CategoryName");
+            System.out.println("Category Name for MainId 122: " + categoryName);
+        } else {
+            System.out.println("MainId 122 not found in the data.");
+        }
+    }
+}
+
+
+
 
 <dependency>
     <groupId>org.json</groupId>
